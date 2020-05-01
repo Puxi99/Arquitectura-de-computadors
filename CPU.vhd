d23d782b6dc8ddc5abd04e5c37aab938090da5c7
jeims99 <jaumeserraa@gmail.com>
2020-05-01 08:52:30 +0200

    Create CPU.vhd
    Mòdul de la CPU

---
+++ CPU.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity CPU is
+    Port(
+        clock,reset: in std_logic;
+    )
+end entity;
+
+architecture behavior of CPU is
+    begin
+        
+    end behavior;
